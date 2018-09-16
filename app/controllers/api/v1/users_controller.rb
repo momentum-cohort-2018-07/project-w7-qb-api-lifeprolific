491cc8161c0ef4771c2aca6e1a40a27400c7b88f
lifeprolific <thegregoman@gmail.com>
2018-09-16 19:08:24 -0400

    added users/show action and view

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::UsersController < ApplicationController
 
+  before_action :set_user, only: [:show, :update, :destroy]
+
   def request_token
     @user = User.find_by_username(params[:username])
     if @user && @user.authenticate(params[:password]) then
@@ -7,12 +9,22 @@ class Api::V1::UsersController < ApplicationController
           :status => :authorized,
           :token => @user.api_token
       }
-  else
-      render :json => {
-          :status => :unauthorized,
-          :token => :forbidden
-      }
+    else
+        render :json => {
+            :status => :unauthorized,
+            :token => :forbidden
+        }
+    end
   end
+
+  def show
+    @questions = @user.questions
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
