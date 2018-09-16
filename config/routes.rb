7f9c57b0689e40700def7e4b322e8dd49c32ed31
lifeprolific <thegregoman@gmail.com>
2018-09-16 17:25:49 -0400

    added API routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1 do
+      resources :questions do
+        resources :answers do
+          resources :votes, only: [:create, :destroy]
+        end
+      end
+      resources :users
+    end
+  end
+
   resources :questions do
     resources :answers do
       resources :votes, only: [:create, :destroy]
@@ -7,5 +19,6 @@ Rails.application.routes.draw do
   resources :users
   resource :session, only: [:new, :create, :destroy]
   root 'questions#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
