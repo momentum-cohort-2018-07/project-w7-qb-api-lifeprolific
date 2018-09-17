3842426b701c8effceb7f74c2e2b6f5af8373b2a
lifeprolific <thegregoman@gmail.com>
2018-09-17 10:04:42 -0400

    removed some debug puts

--- app/controllers/api/v1/votes_controller.rb
+++ app/controllers/api/v1/votes_controller.rb
@@ -1,11 +1,8 @@
 class Api::V1::VotesController < ApplicationController
 
   def create
-    puts "********** hit create **********"
     if authenticate_token
-      puts "********** authenticated **********"
       @vote = Vote.find_by user_id: authenticate_token.id, answer_id: params[:answer_id]
-      puts "********** vote: " + @vote.to_s
       if @vote
         if @vote.update(params.permit(:value, :answer_id).merge({user_id: authenticate_token.id}))
           render :json => {
