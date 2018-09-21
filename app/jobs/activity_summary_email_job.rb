2b9790ea2e7126aa7e2a95a7d41dff32fefdb388
lifeprolific <thegregoman@gmail.com>
2018-09-20 22:40:31 -0400

    added mailer action and views

--- app/jobs/activity_summary_email_job.rb
+++ app/jobs/activity_summary_email_job.rb
@@ -1,7 +1,12 @@
 class ActivitySummaryEmailJob < ApplicationJob
   queue_as :default
 
-  def perform(activities)
-    puts activities
+  def perform(*args)
+    User.all.first do |user|
+      puts "inside loop"
+      UserMailer.activity_summary_email.deliver_later
+    end
+    puts "after loop"
+    # UserMailer.activity_summary_email.deliver_later
   end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,9 @@ class UserMailer < ApplicationMailer
     @url  = params[:url]
     mail(to: @user.email, subject: 'Ahoy, '+@user.username.to_s+'!')
   end
+
+  def activity_summary_email
+    mail(to: "gregorysdtaylor@gmail.com", subject: 'lifo activity summary')
+  end
+
 end
